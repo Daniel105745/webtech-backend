15f430e4f0076aec482b16f698b536890841e8ad
Daniel <liss.daniel@gmx.de>
2025-11-08 19:28:41 +0100

    Milestone 3

--- src/main/java/htw/webtech/myapp/rest/controller/TrainingPlanController.java
+++ src/main/java/htw/webtech/myapp/rest/controller/TrainingPlanController.java
@@ -3,12 +3,14 @@ package htw.webtech.myapp.rest.controller;
 import htw.webtech.myapp.business.service.TrainingPlanService;
 import htw.webtech.myapp.rest.model.TrainingPlanDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://webtech-frontend-fe2m.onrender.com") // erlaubt NUR dein Frontend
 public class TrainingPlanController {
 
     private final TrainingPlanService trainingPlanService;
@@ -19,7 +21,12 @@ public class TrainingPlanController {
 
     @GetMapping("/plans")
     public ResponseEntity<List<TrainingPlanDTO>> getTrainingPlans() {
-
         return ResponseEntity.ok(trainingPlanService.getAllTrainingPlans());
+    }
+
+    // optionaler Test-Endpoint, um zu prüfen, ob dein Backend läuft
+    @GetMapping("/")
+    public String home() {
+        return "Backend läuft auf Render ✅";
     }
 }
