f666185145153ae94eac34ab14787cee8e695b39
Daniel <liss.daniel@gmx.de>
2025-11-09 00:32:53 +0100

    Test

--- src/main/java/htw/webtech/myapp/persistence/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package htw.webtech.myapp.persistence.repository;
-
-import htw.webtech.myapp.persistence.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
--- src/test/java/htw/webtech/myapp/MyappApplicationTests.java
+++ src/test/java/htw/webtech/myapp/MyappApplicationTests.java
@@ -2,6 +2,8 @@ package htw.webtech.myapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
+
 
 @SpringBootTest
 class MyappApplicationTests {
